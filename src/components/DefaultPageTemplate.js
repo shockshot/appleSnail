126094e229969079c1c2b7f78e1d06d178eb65be
JKim <shockshot@naver.com>
2018-04-08 21:24:22 +0900

    scss 적용

--- src/components/DefaultPageTemplate.js
+++ src/components/DefaultPageTemplate.js
@@ -1,8 +1,6 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
-
 
 const DefaultPageTemplate = ({children}) => 
           <div>
@@ -11,25 +9,4 @@ const DefaultPageTemplate = ({children}) =>
               <Footer />
           </div>
 
-/*
-class DefaultPageTemplate extends Component {
-    
-    children;
-
-    constructor({children}) {
-        super();
-        this.children = children;
-    }
-
-    render() {
-        return (
-          <div>
-              <Header />
-              {this.children}
-              <Footer />
-          </div>
-        );
-    }
-}*/
- 
 export default DefaultPageTemplate;
