e4efc644d8694a6d65f6497b1d231ee668eb535b
JKim <shockshot@naver.com>
2018-04-17 23:29:59 +0900

    .

--- server/src/main.js
+++ server/src/main.js
@@ -3,6 +3,7 @@
 import express from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
+import api from './routes';
 
 const app = express();
 const port = 4000;
@@ -12,22 +13,8 @@ app.use(bodyParser.json());
 
 app.use('/', express.static(path.join(__dirname, './../public')));
 
-app.get('/api', (req, res) => {
-    return res.send('Hello world');
-});
+app.use('/api', api);
 
-app.post('/api/users/login', (req, res) => {
-    
-    console.log("req:", req.body);
-
-    const userInfo = {
-        userNo : 1,
-        userId : 'abcde@navar.com',
-        userName : '홍길동'
-    }
-
-    return res.send(JSON.stringify(userInfo));
-});
 
 app.listen(port, () => {
     console.log('Express is listening on port', port);
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -4,4 +4,21 @@ const router = express.Router();
 
 router.post('/', (req, res) => res.json({success:true}) );
 
+router.get('/', (req, res) => {
+  return res.send('Hello world');
+});
+
+router.post('/users/login', (req, res) => {
+  
+  console.log("req:", req.body);
+
+  const userInfo = {
+      userNo : 1,
+      userId : 'abcde@navar.com',
+      userName : '홍길동'
+  }
+
+  return res.send(JSON.stringify(userInfo));
+});
+
 export default router;
