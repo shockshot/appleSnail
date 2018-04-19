798512b798cce9a32970beaa1886e20ac5048e63
ninanino <shockshot@naver.com>
2018-04-19 16:20:36 +0900

    토큰 발급 부분 추가

--- server/src/controllers/Users.js
+++ server/src/controllers/Users.js
@@ -1,19 +1,32 @@
 import db from '../models';
 
-
+const env = process.env.NODE_ENV || "development";
+const jwt = require('jsonwebtoken');
+const config = require('../../config/config.json')[env].jwt;
 
 export const login = (req, res) => {
-  db.User.findAll().then( result => {
-    const users = result.map(data=>data.dataValues);
-
-    return res.send(JSON.stringify(users));
-
-  });
-
-  // const userInfo = {
-  //   userNo : 1,
-  //   userId : 'abcde@navar.com',
-  //   userName : '홍길동'
-  // }
   
+  // console.log('config', config);
+
+  const {userId, password} = req.body;
+
+  db.User.findAll().then( result => {
+    const users = result
+      .map(data=>data.dataValues)
+    ;
+    if(users && users.length){
+      return users[0];
+    }
+
+  }, err => { return res.send(err); } ).then(user => {
+    const payload = {
+      id: user.userId,
+    };
+
+    const token = jwt.sign(payload, config.secret, config.options);
+    // return res.send(JSON.stringify(token));
+    return res.send(token);
+  }, err => {return res.send(err);})
+
 }
+
