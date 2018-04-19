098f6aea1662c0e003c6eba62f58aaf043837697
ninanino <shockshot@naver.com>
2018-04-19 17:27:21 +0900

    .

--- server/src/controllers/Users.js
+++ server/src/controllers/Users.js
@@ -4,13 +4,24 @@ const env = process.env.NODE_ENV || "development";
 const jwt = require('jsonwebtoken');
 const config = require('../../config/config.json')[env].jwt;
 
+const errHandler = (res, err) => {
+  return res.send(err);
+}
+
 export const login = (req, res) => {
   
   // console.log('config', config);
 
-  const {userId, password} = req.body;
+  // const {userId, password} = req.body;
+  const {userId, password} = {userId:'admin', password: null}
 
-  db.User.findAll().then( result => {
+  db.User.findAll({
+    where: {
+      userId: userId,
+      password: password
+    }
+  })
+  .then( result => {
     const users = result
       .map(data=>data.dataValues)
     ;
@@ -18,15 +29,22 @@ export const login = (req, res) => {
       return users[0];
     }
 
-  }, err => { return res.send(err); } ).then(user => {
+  }, err => errHandler(res, err) )
+  .then(user => {
     const payload = {
       id: user.userId,
     };
 
-    const token = jwt.sign(payload, config.secret, config.options);
+    const pr = new Promise((resolve, err) => jwt.sign(payload, config.secret, config.options, (err, token) => {
+      if (err) reject(err)
+      resolve(token) 
+    }));
     // return res.send(JSON.stringify(token));
+    return pr;
+  }, err => errHandler(res, err) )
+  .then( token => {
     return res.send(token);
-  }, err => {return res.send(err);})
+  }, err => errHandler(res, err))
 
 }
 
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -4,7 +4,7 @@ import {login} from '../controllers/users';
 
 const router = express.Router();
 
-router.post('/', (req, res) => res.json({success:true}) );
+// router.post('/', (req, res) => res.json({success:true}) );
 
 router.get('/', (req, res) => {
   return res.send('Hello world');
