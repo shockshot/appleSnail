726c050b68d2e06e663d013a2b7d549ab4b1b630
ninanino <shockshot@naver.com>
2018-04-20 16:29:48 +0900

    토큰 발급 로직 완료

--- server/src/controllers/Users.js
+++ server/src/controllers/Users.js
@@ -4,126 +4,53 @@
 import express from 'express';
 import db from '../models';
 import bodyParser from 'body-parser';
+import passport from '../services/passport';
 
 const router = express.Router();
-router.use(bodyParser.json());
-
 const env = process.env.NODE_ENV || "development";
-const jwt = require('jsonwebtoken');
 const config = require('../../config/config.json')[env].jwt;
+const jwt = require('jsonwebtoken');
 
 
-/**passport */
-
-const passport = require('passport');
-const Strategy = require('passport-local').Strategy;
-
-passport.use(new Strategy({
-    usernameField: 'userId',
-    passwordField: 'password',
-    session: false, // 세션에 저장 여부
-    passReqToCallback: false
-  },
-  function(userId, password, done) {
-    db.User.findOne({
-      where: {
-        userId: userId
-      }
-    })
-    .then( result => {
-      if(!result) {
-        return done(null, false);
-      }
-
-      const user = result.dataValues;
-      if(user.password !== password){
-        return done(null, false);
-      }
-
-      const payload = {
-        id: user.userId
-      };
-      const token = jwt.sign(payload, config.secret, config.options);
-
-      return done(null, token);
-
-    }, err => done(err) );
-  }
-));
-
-passport.serializeUser(function(user, cb) {
-  console.log('serializeUser');
-  cb(null, user);
+/** middlewares */
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended: true}));
+router.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'content-type, x-access-token'); //1
+  next();
 });
 
-passport.deserializeUser(function(user, cb) {
-  console.log('deserial');
-  db.Users.find({
-    where: {
-      userId: user.userId
-    }
-  }).then(user => {
-    cb(null, user.dataValues);
-  }, err=>{
-    console.log('deserializeUser err', err);
-    cb(err);
-  })
-});
-
-
+//init passport
 router.use(passport.initialize());
-// router.use(passport.session());
 
 
 const errHandler = (res, err) => {
   res.status(200).send('err:'+err);
 }
 
+
 router.post('/login', passport.authenticate('local', {
   // successRedirect: '/',
   // failureRedirect: '/',
   // failureFlash: true
 }), (req, res) => {
-  res.send('login success');
+
+  const user = req.user;
+  // issuing token.
+  const payload = {
+    id: user.userId
+  };
+  const token = jwt.sign(payload, config.secret, config.options);
+  req._token = token;
+
+  console.log('#token:', token);
+  
+  res.setHeader('getToken', 'ok')
+  res.status(200).send(token);
+  
 });
 
-/*
-
-router.post('/login', (req, res)=>{
-  const {userId, password} = req.body;
-  console.log(req.body);
-
-  db.User.findOne({
-      where: {
-        userId: userId,
-        password: password
-      }
-    })
-    .then( result => {
-
-      if(!result){
-        throw new Error('user not found');
-      }
-
-      const user = result.dataValues;
-      const payload = {
-        id: user.userId,
-      };
-
-      const pr = new Promise((resolve, err) => jwt.sign(payload, config.secret, config.options, (err, token) => {
-        if (err) reject(err)
-        resolve(token) 
-      }));
-      // return res.send(JSON.stringify(token));
-      return pr;
-  
-    }, err => errHandler(res, err) )
-    .then( token => {
-      return res.send(token);
-    }, err => errHandler(res, err))
-
-  }
-);
-*/
 
 export default router;
---
+++ server/src/services/passport.js
@@ -0,0 +1,69 @@
+import db from '../models';
+import bcrypt from 'bcrypt';
+
+/**passport */
+const passport = require('passport');
+const Strategy = require('passport-local').Strategy;
+
+const hashRounds = 10;
+
+/**메인 로그인 로직 */
+const loginLogic = function(req, userId, password, done) {
+  let user;
+  db.User.findOne({
+    where: {
+      userId: userId
+    }
+  })
+  .then( result => {
+    if(!result) {
+      return done(null, false);
+    }
+
+    user = result.dataValues;
+
+    return bcrypt.compare(password, user.password)
+  }, err => done(err) )
+  .then( res => {
+    if(!res) {
+      return done(null, false);
+    }else{
+      return done(null, user);
+    }
+  })
+}
+
+
+passport.use(new Strategy({
+    usernameField: 'userId',
+    passwordField: 'password',
+    session: false, // 세션에 저장 여부
+    passReqToCallback: true
+  },loginLogic
+));
+
+//로그인 성공시 실행
+passport.serializeUser(function(user, done) {
+  // console.log('serializeUser. user:', user);
+  done(null, user);
+});
+
+//서버로 들어오는 요청마다 실제 DB와 비교
+passport.deserializeUser(function(user, cb) {
+  // console.log('deserial');
+  db.Users.find({
+    where: {
+      userId: user.userId
+    }
+  }).then(user => {
+    cb(null, user.dataValues);
+  }, err=>{
+    // console.log('deserializeUser err', err);
+    cb(err);
+  })
+});
+
+
+// router.use(passport.session());
+
+export default passport;
