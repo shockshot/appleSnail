8b23df48c721aee47094879a3f70a24e261a7a83
JKim <shockshot@naver.com>
2018-04-22 17:21:35 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Component } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { Router } from 'react-router';
 import { Route, Switch } from 'react-router-dom';
 import { history } from './helper/history';
