6aee536f3ff94fe4117944a4c3d53a2b7a983850
ninanino <shockshot@naver.com>
2018-04-25 10:40:59 +0900

    .

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import './index.css';
 import store from './helper/store'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 //common css
+import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
