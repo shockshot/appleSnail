460ac13c64820b1dd36d1f8505dc3afaffc8b835
ninanino <shockshot@naver.com>
2018-04-25 11:06:03 +0900

    .

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -3,33 +3,18 @@ import _DatePicker from 'react-datepicker';
 import moment from 'moment';
 import 'react-datepicker/dist/react-datepicker.css';
 
-const defaultDateFormat = 'YYYYMMDD';
-
-class DatePicker extends Component {
-  constructor(props) {
-    super(props);
-    this.dateFormat = props.dateFormat ? props.dateFormat : defaultDateFormat;
-  }
-
-  onChangeHandler = (e) => {
-    this.props.onChange({
-      selectedDate: e.format(this.dateFormat),
-      name: this.props.name
+const DatePicker = ({dateFormat = 'YYYYMMDD', name, selected, onChange = f=>f}) =>
+<_DatePicker 
+  className="form-control" 
+  dateFormat={dateFormat}
+  selected={moment(selected, dateFormat)}
+  onChange={(e) => {
+    onChange({
+      selectedDate: e.format(dateFormat),
+      name: name
     });
-  }
-
-  render(){
-    return (
-      <_DatePicker 
-        className="form-control" 
-        dateFormat={this.dateFormat}
-        selected={moment(this.props.selected, this.dateFormat)}
-        onChange={this.onChangeHandler}
-        name={this.props.name}
-        />
-    )
-  }
-}
-
+  }}
+  name={name}
+/>
 
 export default DatePicker;
