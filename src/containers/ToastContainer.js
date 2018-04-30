02088c37d1fbf9d98343360e67dab65b6a67ecc5
ninanino <shockshot@naver.com>
2018-04-30 17:02:18 +0900

    .

---
+++ src/containers/ToastContainer.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import { Logger } from 'helpers';
+
+
+class ToastContainer extends Component {
+
+  render(){
+    return(
+      <div></div>
+    )
+  }
+}
+
+export default ToastContainer;
